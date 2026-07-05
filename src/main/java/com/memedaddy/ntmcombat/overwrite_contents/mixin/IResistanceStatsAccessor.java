d4d614c0df0bd721b656fedb74e406687ff59816
memedaddy69 <eduardoalfredo20133@gmail.com>
2026-07-05 17:24:53 -0600

    Add imports and update accessor for other resistance

--- src/main/java/com/memedaddy/ntmcombat/overwrite_contents/mixin/IResistanceStatsAccessor.java
+++ src/main/java/com/memedaddy/ntmcombat/overwrite_contents/mixin/IResistanceStatsAccessor.java
@@ -1,5 +1,10 @@
 package com.memedaddy.ntmcombat.overwrite_contents.mixin;
 
+import com.hbm.util.DamageResistanceHandler;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.gen.Accessor;
+import java.util.HashMap;
+
 @Mixin(DamageResistanceHandler.ResistanceStats.class)
 public interface IResistanceStatsAccessor {
     @Accessor(value = "exactResistances", remap = false)
@@ -7,4 +12,7 @@ public interface IResistanceStatsAccessor {
 
     @Accessor(value = "categoryResistances", remap = false)
     HashMap<String, DamageResistanceHandler.Resistance> getCategoryResistances();
+
+    @Accessor(value = "otherResistance", remap = false)
+    DamageResistanceHandler.Resistance getOtherResistance();
 }
